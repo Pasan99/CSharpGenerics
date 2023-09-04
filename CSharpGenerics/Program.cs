8377ced66410de05badbdc224f9297a6c64d07ff
Pasan99 <pasanmanohara2000@gmail.com>
2023-09-04 20:09:14 +0530

    Add project files.

---
+++ CSharpGenerics/Program.cs
@@ -0,0 +1,11 @@
+﻿using CSharpGenerics.Models;
+using CSharpGenerics.Utilities;
+
+Console.WriteLine("Press enter to read the test.csv file");
+Console.ReadLine();
+var users = CsvFileReader.Read<User>("test.csv");
+
+foreach (var user in users)
+{
+    Console.WriteLine($"Name : {user.Name}, Email : {user.Email}");
+}
